dbe91a2304e8ce8548075a68d969deff58f4d7d1
xlz122 <122874722@qq.com>
2022-10-28 16:51:08 +0800

    feat: 增加直播首页接口

---
+++ module/live_index.js
@@ -0,0 +1,15 @@
+// 直播首页
+
+module.exports = (query, request) => {
+  return request(
+    'GET',
+    `https://api.live.bilibili.com/room/v2/AppIndex/getAllList?device=phone&platform=ios&scale=3&build=10000&protocol=0,1&format=0,1,2&codec=0,1`,
+    {},
+    {
+      crypto: 'm',
+      cookie: query.cookie,
+      proxy: query.proxy,
+      realIP: query.realIP
+    }
+  );
+};
